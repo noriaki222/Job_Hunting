acc4ea8e0268bd064e52b3b3ed8fb24b9d77899c
noriaki222 <osakinoriaki222@gmail.com>
2022-06-03 11:00:29 +0900

    CreateWindow

--- DX11Game/main.cpp
+++ DX11Game/main.cpp
@@ -42,5 +42,413 @@ int							g_nCountFPS;			// FPSカウンタ
 // メイン関数
 int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCmdShow)
 {
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+
+	DWORD dwExecLastTime;
+	DWORD dwFPSLastTime;
+	DWORD dwCurrentTime;
+	DWORD dwFrameCount;
+
+	WNDCLASSEX wcex = { sizeof(WNDCLASSEX), CS_CLASSDC, WndProc, 0, 0, hInstance, nullptr,
+	LoadCursor(nullptr, IDC_ARROW), (HBRUSH)(COLOR_WINDOW + 1), nullptr, CLASS_NAME, nullptr };
+
+	MSG msg;
+
+	// COM初期化
+	if (FAILED(CoInitializeEx(nullptr, COINIT_MULTITHREADED))) 
+	{
+		MessageBox(NULL, _T("COMの初期化に失敗しました。"), _T("error"), MB_OK);
+		return -1;
+	}
+
+	// インスタンス ハンドル保存
+	g_hInst = hInstance;
+
+	// ウィンドウクラスの登録
+	RegisterClassEx(&wcex);
+
+	// ウィンドウ サイズ算出
+	DWORD dwStyle = WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_BORDER | WS_MINIMIZEBOX;
+	DWORD dwExStyle = 0;
+	RECT rc = { 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT };
+	AdjustWindowRectEx(&rc, dwStyle, FALSE, dwExStyle);
+
+	// ウィンドウの作成
+	g_hWnd = CreateWindowEx(dwExStyle, CLASS_NAME, WINDOW_NAME, dwStyle, CW_USEDEFAULT, CW_USEDEFAULT,
+		rc.right - rc.left, rc.bottom - rc.top,	nullptr, nullptr, hInstance, nullptr);
+
+	// フレームカウント初期化
+	timeBeginPeriod(1);				// 分解能を設定
+	dwExecLastTime = dwFPSLastTime = timeGetTime();
+	dwCurrentTime = dwFrameCount = 0;
+
+	// ウインドウの表示
+	ShowWindow(g_hWnd, nCmdShow);
+	UpdateWindow(g_hWnd);
+
+	// DirectXの初期化
+	if (FAILED(Init(g_hWnd, true))) { return -1;}
+
+	// メッセージループ
+	for (;;) 
+	{
+		if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE)) 
+		{
+			if (msg.message == WM_QUIT) 
+			{
+				// PostQuitMessage()が呼ばれたらループ終了
+				break;
+			}
+			else 
+			{
+				// メッセージの翻訳とディスパッチ
+				TranslateMessage(&msg);
+				DispatchMessage(&msg);
+			}
+		}
+		else 
+		{
+			dwCurrentTime = timeGetTime();
+			if ((dwCurrentTime - dwFPSLastTime) >= 500) 
+			{	
+				// 0.5秒ごとに実行
+				g_nCountFPS = dwFrameCount * 1000 / (dwCurrentTime - dwFPSLastTime);
+				dwFPSLastTime = dwCurrentTime;
+				dwFrameCount = 0;
+			}
+			if ((dwCurrentTime - dwExecLastTime) >= (1000 / 60)) 
+			{
+				dwExecLastTime = dwCurrentTime;
+
+				// 更新処理
+				Update();
+			}
+
+			// 描画処理
+			Draw();
+			dwFrameCount++;
+		}
+	}
+
+	// タイマ設定を元に戻す
+	timeEndPeriod(1);
+
+	// 終了処理
+	Uninit();
+
+	// ウィンドウクラスの登録を解除
+	UnregisterClass(CLASS_NAME, g_hInst);
+
+	// COM終了処理
+	CoUninitialize();
+
+	return (int)msg.wParam;
+}
+
+// プロシージャ
+LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_CREATE:					
+		// ウィンドウが生成された
+		return OnCreate(hWnd, (LPCREATESTRUCT)lParam);
+	case WM_DESTROY:				
+		// ウィンドウ破棄指示がきた
+		PostQuitMessage(0);				// システムにスレッドの終了
+		break;
+	case WM_KEYDOWN:				
+		// キーボードが押された
+		switch (wParam) 
+		{
+		case VK_ESCAPE:
+			// [x]が押されたように振舞う
+			PostMessage(hWnd, WM_CLOSE, 0, 0);
+			return 0;
+		}
+		break;
+	case WM_MENUCHAR:
+		return MNC_CLOSE << 16;			// [Alt]+[Enter]時のBEEPを抑止
+	default:
+		break;
+	}
+
+	return DefWindowProc(hWnd, uMsg, wParam, lParam);
+}
+
+// メッセージハンドラ
+int OnCreate(HWND hWnd, LPCREATESTRUCT lpcs)
+{
+	// クライアント領域サイズを再設定
+	RECT rcClnt;
+	GetClientRect(hWnd, &rcClnt);
+	rcClnt.right -= rcClnt.left;
+	rcClnt.bottom -= rcClnt.top;
+	if (rcClnt.right != SCREEN_WIDTH || rcClnt.bottom != SCREEN_HEIGHT) 
+	{
+		RECT rcWnd;
+		GetWindowRect(hWnd, &rcWnd);
+		SIZE sizeWnd;
+		sizeWnd.cx = (rcWnd.right - rcWnd.left) - rcClnt.right + SCREEN_WIDTH;
+		sizeWnd.cy = (rcWnd.bottom - rcWnd.top) - rcClnt.bottom + SCREEN_HEIGHT;
+		SetWindowPos(hWnd, nullptr, 0, 0, sizeWnd.cx, sizeWnd.cy, SWP_NOMOVE | SWP_NOZORDER | SWP_NOOWNERZORDER);
+	}
+
+	// IME無効化
+	ImmAssociateContext(hWnd, nullptr);
+
 	return 0;
+}
+
+// バックバッファ生成
+HRESULT CreateBackBuffer(void)
+{
+	// レンダーターゲットビュー生成
+	ID3D11Texture2D* pBackBuffer = nullptr;
+	g_pSwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID*)&pBackBuffer);
+	g_pDevice->CreateRenderTargetView(pBackBuffer, nullptr, &g_pRenderTargetView);
+	SAFE_RELEASE(pBackBuffer);
+
+	// Zバッファ用テクスチャ生成
+	D3D11_TEXTURE2D_DESC td;
+	ZeroMemory(&td, sizeof(td));
+	td.Width = SCREEN_WIDTH;
+	td.Height = SCREEN_HEIGHT;
+	td.MipLevels = 1;
+	td.ArraySize = 1;
+	td.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;
+	td.SampleDesc.Count = 1;
+	td.SampleDesc.Quality = 0;
+	td.Usage = D3D11_USAGE_DEFAULT;
+	td.BindFlags = D3D11_BIND_DEPTH_STENCIL;
+	HRESULT hr = g_pDevice->CreateTexture2D(&td, nullptr, &g_pDepthStencilTexture);
+	if (FAILED(hr)) { return hr; }
+
+	// Zバッファターゲットビュー生成
+	D3D11_DEPTH_STENCIL_VIEW_DESC dsvd;
+	ZeroMemory(&dsvd, sizeof(dsvd));
+	dsvd.Format = td.Format;
+	dsvd.ViewDimension = D3D11_DSV_DIMENSION_TEXTURE2DMS;
+	hr = g_pDevice->CreateDepthStencilView(g_pDepthStencilTexture,
+		&dsvd, &g_pDepthStencilView);
+	if (FAILED(hr)) { return hr; }
+
+	// 各ターゲットビューをレンダーターゲットに設定
+	g_pDeviceContext->OMSetRenderTargets(1, &g_pRenderTargetView, g_pDepthStencilView);
+
+	// ビューポート設定
+	D3D11_VIEWPORT vp;
+	vp.Width = (float)SCREEN_WIDTH;
+	vp.Height = (float)SCREEN_HEIGHT;
+	vp.MinDepth = 0.0f;
+	vp.MaxDepth = 1.0f;
+	vp.TopLeftX = 0;
+	vp.TopLeftY = 0;
+	g_pDeviceContext->RSSetViewports(1, &vp);
+
+	return S_OK;
+}
+
+// 初期化処理
+HRESULT Init(HWND hWnd, BOOL bWindow)
+{
+	HRESULT hr = S_OK;
+
+	// デバイス、スワップチェーンの作成
+	DXGI_SWAP_CHAIN_DESC scd;
+	ZeroMemory(&scd, sizeof(scd));
+	scd.BufferCount = 1;
+	scd.BufferDesc.Width = SCREEN_WIDTH;
+	scd.BufferDesc.Height = SCREEN_HEIGHT;
+	scd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	scd.BufferDesc.RefreshRate.Numerator = 60;
+	scd.BufferDesc.RefreshRate.Denominator = 1;
+	scd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	scd.OutputWindow = hWnd;
+	scd.SampleDesc.Count = 1;
+	scd.SampleDesc.Quality = 0;
+	scd.Windowed = bWindow;
+
+	D3D_FEATURE_LEVEL featureLevels[] = 
+	{
+		D3D_FEATURE_LEVEL_11_1,
+		D3D_FEATURE_LEVEL_11_0,
+		D3D_FEATURE_LEVEL_10_1,
+		D3D_FEATURE_LEVEL_10_0,
+		D3D_FEATURE_LEVEL_9_3,
+		D3D_FEATURE_LEVEL_9_2,
+		D3D_FEATURE_LEVEL_9_1,
+	};
+
+	hr = D3D11CreateDeviceAndSwapChain(nullptr, D3D_DRIVER_TYPE_HARDWARE,
+		nullptr, 0, featureLevels, _countof(featureLevels), D3D11_SDK_VERSION, &scd,
+		&g_pSwapChain, &g_pDevice, nullptr, &g_pDeviceContext);
+	if (FAILED(hr)) { return hr; }
+
+	// バックバッファ生成
+	hr = CreateBackBuffer();
+	if (FAILED(hr)) { return hr; }
+
+	// ラスタライズ設定
+	D3D11_RASTERIZER_DESC rd;
+	ZeroMemory(&rd, sizeof(rd));
+	rd.FillMode = D3D11_FILL_SOLID;
+	rd.CullMode = D3D11_CULL_NONE;	// カリング無し(両面描画)
+	g_pDevice->CreateRasterizerState(&rd, &g_pRs[0]);
+	rd.CullMode = D3D11_CULL_FRONT;	// 前面カリング(裏面描画)
+	g_pDevice->CreateRasterizerState(&rd, &g_pRs[1]);
+	rd.CullMode = D3D11_CULL_BACK;	// 背面カリング(表面描画)
+	g_pDevice->CreateRasterizerState(&rd, &g_pRs[2]);
+	g_pDeviceContext->RSSetState(g_pRs[2]);
+
+	// ブレンド ステート生成
+	D3D11_BLEND_DESC BlendDesc;
+	ZeroMemory(&BlendDesc, sizeof(BlendDesc));
+	BlendDesc.AlphaToCoverageEnable = FALSE;
+	BlendDesc.IndependentBlendEnable = FALSE;
+	BlendDesc.RenderTarget[0].BlendEnable = FALSE;
+	BlendDesc.RenderTarget[0].SrcBlend = D3D11_BLEND_SRC_ALPHA;
+	BlendDesc.RenderTarget[0].DestBlend = D3D11_BLEND_INV_SRC_ALPHA;
+	BlendDesc.RenderTarget[0].BlendOp = D3D11_BLEND_OP_ADD;
+	BlendDesc.RenderTarget[0].SrcBlendAlpha = D3D11_BLEND_ONE;
+	BlendDesc.RenderTarget[0].DestBlendAlpha = D3D11_BLEND_ZERO;
+	BlendDesc.RenderTarget[0].BlendOpAlpha = D3D11_BLEND_OP_ADD;
+	BlendDesc.RenderTarget[0].RenderTargetWriteMask = D3D11_COLOR_WRITE_ENABLE_ALL;
+	g_pDevice->CreateBlendState(&BlendDesc, &g_pBlendState[0]);
+	// ブレンド ステート生成 (アルファ ブレンド用)
+	//BlendDesc.AlphaToCoverageEnable = TRUE;
+	BlendDesc.RenderTarget[0].BlendEnable = TRUE;
+	g_pDevice->CreateBlendState(&BlendDesc, &g_pBlendState[1]);
+	// ブレンド ステート生成 (加算合成用)
+	BlendDesc.RenderTarget[0].DestBlend = D3D11_BLEND_ONE;
+	g_pDevice->CreateBlendState(&BlendDesc, &g_pBlendState[2]);
+	// ブレンド ステート生成 (減算合成用)
+	BlendDesc.RenderTarget[0].BlendOp = D3D11_BLEND_OP_REV_SUBTRACT;
+	g_pDevice->CreateBlendState(&BlendDesc, &g_pBlendState[3]);
+	SetBlendState(BS_ALPHABLEND);
+
+	// 深度ステンシルステート生成
+	CD3D11_DEFAULT def;
+	CD3D11_DEPTH_STENCIL_DESC dsd(def);
+	dsd.DepthWriteMask = D3D11_DEPTH_WRITE_MASK_ZERO;
+	g_pDevice->CreateDepthStencilState(&dsd, &g_pDSS[0]);
+	CD3D11_DEPTH_STENCIL_DESC dsd2(def);
+	dsd2.DepthEnable = FALSE;
+	g_pDevice->CreateDepthStencilState(&dsd2, &g_pDSS[1]);
+
+	// 以下各クラスの初期化処理を書く
+
+	return hr;
+}
+
+// バックバッファ解放
+void ReleaseBackBuffer()
+{
+	if (g_pDeviceContext) {	g_pDeviceContext->OMSetRenderTargets(0, nullptr, nullptr); }
+	SAFE_RELEASE(g_pDepthStencilView);
+	SAFE_RELEASE(g_pDepthStencilTexture);
+	SAFE_RELEASE(g_pRenderTargetView);
+}
+
+// 終了処理
+void Uninit(void)
+{
+	// 以下各クラスの初期化処理を書く
+
+	// 深度ステンシルステート解放
+	for (int i = 0; i < _countof(g_pDSS); ++i) {
+		SAFE_RELEASE(g_pDSS[i]);
+	}
+
+	// ブレンド ステート解放
+	for (int i = 0; i < MAX_BLENDSTATE; ++i) {
+		SAFE_RELEASE(g_pBlendState[i]);
+	}
+
+	// ラスタライザ ステート解放
+	for (int i = 0; i < MAX_CULLMODE; ++i) {
+		SAFE_RELEASE(g_pRs[i]);
+	}
+
+	// バックバッファ解放
+	ReleaseBackBuffer();
+
+	// スワップチェーン解放
+	SAFE_RELEASE(g_pSwapChain);
+
+	// デバイス コンテキストの開放
+	SAFE_RELEASE(g_pDeviceContext);
+
+	// デバイスの開放
+	SAFE_RELEASE(g_pDevice);
+}
+
+// 更新処理
+void Update(void)
+{
+	// 各クラスの更新処理を書く
+
+}
+
+// 描画処理
+void Draw(void)
+{
+	// 各クラスの描画処理を書く
+
+	// バックバッファとフロントバッファの入れ替え
+	g_pSwapChain->Present(g_uSyncInterval, 0);
+}
+
+// メインウィンドウハンドル取得
+HWND GetMainWnd()
+{
+	return g_hWnd;
+}
+
+// インスタンスハンドル取得
+HINSTANCE GetInstance()
+{
+	return g_hInst;
+}
+
+// デバイス取得
+ID3D11Device* GetDevice()
+{
+	return g_pDevice;
+}
+
+// デバイスコンテキスト取得
+ID3D11DeviceContext* GetDeviceContext()
+{
+	return g_pDeviceContext;
+}
+
+// Zバッファ有効無効制御
+void SetZBuffer(bool bEnable)
+{
+	g_pDeviceContext->OMSetDepthStencilState((bEnable) ? nullptr : g_pDSS[1], 0);
+}
+
+// Zバッファ更新有効無効制御
+void SetZWrite(bool bEnable)
+{
+	g_pDeviceContext->OMSetDepthStencilState((bEnable) ? nullptr : g_pDSS[0], 0);
+}
+
+// ブレンドステート設定
+void SetBlendState(int nBlendState)
+{
+	if (nBlendState >= 0 && nBlendState < MAX_BLENDSTATE) {
+		float blendFactor[4] = { 0.0f, 0.0f, 0.0f, 0.0f };
+		g_pDeviceContext->OMSetBlendState(g_pBlendState[nBlendState], blendFactor, 0xffffffff);
+	}
+}
+
+// カリング設定
+void SetCullMode(int nCullMode)
+{
+	if (nCullMode >= 0 && nCullMode < MAX_CULLMODE) {
+		g_pDeviceContext->RSSetState(g_pRs[nCullMode]);
+	}
 }
